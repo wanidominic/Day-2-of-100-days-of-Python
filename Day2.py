10588e1c9a25c269d85f9ea0af63e6758d24f599
wanidominic <wanidominic536@gmail.com>
2025-02-12 16:33:26 +0300

    First Commit

---
+++ Day2.py
@@ -0,0 +1,30 @@
+#Python example
+greeting = 'Hello, world!'
+
+print(greeting)
+
+#store as a string
+
+name = "Dominic Wani"
+print(name)
+
+#two numbers and add them together
+num1 = 12
+num2 = 20
+sum_result = num1 + num2
+print("sum:", sum_result)
+
+#store a boolean value and print
+is_wani_a_name = True
+print("boolean vaalue:", is_wani_a_name)
+
+#convert an integer to a string and print its type
+num = 42
+num_str = str(num)
+print("converted number:", num_str)
+print("Type:", type(num_str))
+
+
+#Create a list of 5 favorite foods and print the first one.
+favorite_foods = ["chapati", "ugali", "pizza", "cake", "rice"]
+print("first favorite food:", favorite_foods[0])
